2088bce14732376c6ccd88ead240087fa518a4a4
Katherine Struthers <ktstruthers@gmail.com>
2023-07-11 12:36:38 -0500

    fix left join

--- 1_queries/3_properties_by_city.sql
+++ 1_queries/3_properties_by_city.sql
@@ -1,7 +1,7 @@
 SELECT properties.id, title, cost_per_night, 
   AVG(rating) AS average_rating
 FROM properties
-JOIN property_reviews ON property_id = properties.id
+LEFT JOIN property_reviews ON property_id = properties.id
 WHERE city LIKE '%ancouve%'
 GROUP BY properties.id
 HAVING AVG(rating) >= 4
